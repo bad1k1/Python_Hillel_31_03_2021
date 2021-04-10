98c4b1378efd92ebd84eac950cc1fe024ad359f5
bad1k <bad1k1dp@gmail.com>
2021-04-10 09:21:20 +0300

    Добавление первого проекта в Git

---
+++ Homework_2.py
@@ -0,0 +1,18 @@
+
+value = input("Введи целое число:")
+print(value, type(value))
+result = int(value) * 2
+print(result)
+
+
+#########################################
+value = input("Введи дробное число:")
+print(value, type(value))
+result = float(value) * 2
+print(result)
+
+#########################################
+value = input("Введи строку:")
+print(value, type(value))
+result = str(value) * 2
+print(result)
