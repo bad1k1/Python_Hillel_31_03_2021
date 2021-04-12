653cdbbd3779ef99911e23add2dcca423e05e813
bad1k <bad1k1dp@gmail.com>
2021-04-12 14:49:28 +0300

    ДЗ

---
+++ Homework_3.py
@@ -0,0 +1,42 @@
+###################################exercise_1
+
+value = 100
+new_value = value / 2 if value < 100 else - value
+print(new_value)
+
+###################################exercise_2
+
+value = 1000
+new_value = value = 1 if value < 100 else - 0
+print(new_value)
+
+###################################exercise_3
+
+value = 1000
+new_value = value < 100 if value else - 0
+print(new_value)
+
+###################################exercise_4
+
+my_str = "Homework3"
+print(my_str[::2])
+
+###################################exercise_5
+
+my_str = "Homework3"
+print(my_str[1::2])
+
+###################################exercise_6
+
+my_str = "task"
+new_str = my_str + my_str if len(my_str) < 5 else my_str
+
+print(new_str)
+
+###################################exercise_7
+
+my_str = "task"
+new_str = my_str + my_str[::-1] if len(my_str) < 5 else my_str
+
+print(new_str)
+
